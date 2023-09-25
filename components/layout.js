46785abccfd290e501283484ec05fe1adfc83a10
Jmike8385e <142643946+Jmike8385e@users.noreply.github.com>
2023-09-25 23:30:26 +0000

    Removed second apostrophe

--- components/layout.js
+++ components/layout.js
@@ -22,7 +22,7 @@ export default function Layout( { children, home } ) {
         )
       }
       <footer>
-        <p>Please don't sue me NFL</p>
+        <p>Please do not sue me NFL</p>
       </footer>
     </div>
   );
